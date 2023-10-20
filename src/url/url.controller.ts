337134120fbccec08de261f0fa10107dfac370e7
Blaise NDIZEYE <68238750+blaise-ndizeye@users.noreply.github.com>
2023-10-20 19:14:33 +0200

    Rearrange url.controller code lines

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -9,52 +9,48 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { UserRole } from '@prisma/client';
 import { UrlService } from './url.service';
 import {
   CreateShortenedUrlDto,
   UpdateShortenedUrlDto,
   UrlFiltersDto,
-  UrlResponseDto,
 } from './dtos/url.dto';
 import { User } from 'src/user/decorators/user.decorator';
 import { TokenPayload } from 'src/user/dtos/user.dto';
 import { Roles } from 'src/user/decorators/role.decorator';
-import { UserRole } from '@prisma/client';
 
 @Controller('url')
+@Roles(UserRole.ADMIN, UserRole.USER)
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
-  @Roles(UserRole.USER)
   @Get('list')
   getUserUrls(@User() user: TokenPayload, @Query() filters: UrlFiltersDto) {
     return this.urlService.getUserUrls(user.id, filters);
   }
 
-  @Roles(UserRole.ADMIN, UserRole.USER)
   @Post()
   createShortenedUrl(
-    @Body() body: CreateShortenedUrlDto,
     @User() user: TokenPayload,
+    @Body() body: CreateShortenedUrlDto,
   ) {
     return this.urlService.createShortenedUrl(body, user.id);
   }
 
-  @Roles(UserRole.ADMIN, UserRole.USER)
   @Put(':urlId')
   updateShortenedUrl(
     @Param('urlId', ParseIntPipe) urlId: number,
-    @Body() body: UpdateShortenedUrlDto,
     @User() user: TokenPayload,
+    @Body() body: UpdateShortenedUrlDto,
   ) {
     return this.urlService.updateShortenedUrl(urlId, body, user.id);
   }
 
-  @Roles(UserRole.ADMIN, UserRole.USER)
   @Delete(':urlId')
   deleteShortenedUrl(
-    @Param('urlId', ParseIntPipe) urlId: number,
     @User() user: TokenPayload,
+    @Param('urlId', ParseIntPipe) urlId: number,
   ) {
     return this.urlService.deleteShortenedUrl(urlId, user.id);
   }
