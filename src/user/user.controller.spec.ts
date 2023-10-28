eea6c909c26347280d60c82f9051106d5c1348e8
Blaise NDIZEYE <68238750+blaise-ndizeye@users.noreply.github.com>
2023-10-28 17:07:21 +0200

    create a unit test for signIn controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -3,10 +3,10 @@ import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcryptjs';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { SignUpDto } from './dtos/user.dto';
+import { SignInDto, SignUpDto } from './dtos/user.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserRole } from '@prisma/client';
-import { ConflictException } from '@nestjs/common';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -56,6 +56,40 @@ describe('UserController', () => {
 
       await expect(controller.signUp(signUpDto)).rejects.toThrowError(
         ConflictException,
+      );
+    });
+  });
+
+  describe('SignIn', () => {
+    it('should return a JWT token', async () => {
+      const signInDto: SignInDto = {
+        username: 'testuser',
+        password: 'password123',
+      };
+
+      jest.spyOn(service, 'signIn').mockResolvedValue(expect.any(String));
+
+      const result = await controller.signIn(signInDto);
+
+      expect(result).toEqual(expect.any(String));
+      expect(service.signIn).toHaveBeenCalledWith(signInDto);
+    });
+
+    it('should throw BadRequestException when invalid credentials are provided', async () => {
+      const signInDto: SignInDto = {
+        username: 'testuser',
+        password: 'invalidPassword',
+      };
+
+      jest.spyOn(service, 'findUserByUsername').mockResolvedValue({
+        id: 1,
+        username: signInDto.username,
+        password: 'random password',
+        role: UserRole.USER,
+      });
+
+      await expect(controller.signIn(signInDto)).rejects.toThrowError(
+        BadRequestException,
       );
     });
   });
