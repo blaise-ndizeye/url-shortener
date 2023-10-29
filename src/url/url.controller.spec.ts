9ac37f93833a75ea0b51698d7c59812bdc35145f
Blaise NDIZEYE <68238750+blaise-ndizeye@users.noreply.github.com>
2023-10-29 13:14:50 +0200

    create a unit test for createShortenedUrl controller

--- src/url/url.controller.spec.ts
+++ src/url/url.controller.spec.ts
@@ -3,7 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
 import { PrismaService } from '../../prisma/prisma.service';
-import { UrlResponseDto } from './dtos/url.dto';
+import { CreateShortenedUrlDto, UrlResponseDto } from './dtos/url.dto';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UrlController', () => {
   let controller: UrlController;
@@ -39,6 +40,44 @@ describe('UrlController', () => {
       await expect(
         controller.getUserUrls({ id: 1 }, { expired: true, search: 'ort' }),
       ).resolves.toEqual([data]);
+    });
+  });
+
+  describe('createSHortenedUrl', () => {
+    it('should create shortened url', async () => {
+      const data = new UrlResponseDto({
+        id: 1,
+        originalUrl: 'https://google.com',
+        shortUrl: 'https://short.com',
+        numberOfClicks: 0,
+        expiresAt: new Date(2024, 1, 1),
+        isPasswordProtected: true,
+      });
+
+      jest.spyOn(service, 'createShortenedUrl').mockResolvedValue(data);
+
+      await expect(
+        controller.createShortenedUrl(
+          { id: 1 },
+          {
+            originalUrl: 'https://google.com',
+            expirationDate: new Date(2024, 1, 1),
+            password: '123',
+          },
+        ),
+      ).resolves.toEqual(data);
+    });
+
+    it('should throw BadRequestException when expirationDate is not in future', async () => {
+      const body: CreateShortenedUrlDto = {
+        expirationDate: new Date(),
+        originalUrl: 'https://google.com',
+        password: '123',
+      };
+
+      await expect(
+        controller.createShortenedUrl({ id: 1 }, body),
+      ).rejects.toThrowError(BadRequestException);
     });
   });
 });
