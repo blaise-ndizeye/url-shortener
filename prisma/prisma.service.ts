25e84086333ff9c22edb87987b13dc7dc4859e86
Blaise NDIZEYE <68238750+blaise-ndizeye@users.noreply.github.com>
2023-11-02 13:00:20 +0200

    Revert "Invalid test config"
    This reverts commit b422745c8137e42ea495da58c21800e8667da974.

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -6,15 +6,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor(private readonly dbConnection: string) {
-    super({
-      datasources: {
-        db: {
-          url: dbConnection,
-        },
-      },
-    });
-  }
   async onModuleInit() {
     await this.$connect();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,18 @@ import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaService } from 'prisma/prisma.service';
 import { UserInterceptor } from './user/interceptos/user.interceptor';
 import { AuthGuard } from './user/guards/user.guard';
 import { UrlModule } from './url/url.module';
 import { CustomConfigModule } from './config/config.module';
 
 @Module({
-  imports: [UserModule, UrlModule, CustomConfigModule, PrismaModule],
+  imports: [UserModule, UrlModule, CustomConfigModule],
   controllers: [AppController],
   providers: [
     AppService,
+    PrismaService,
     {
       provide: APP_INTERCEPTOR,
       useClass: UserInterceptor,
